4fd706ee64b0cd932ae8310b2ddac3bce1af0702
root <kariukionesmus01@gmail.com>
2022-09-20 13:57:00 -0700

    Cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,57 +1,31 @@
-nclude "lists.h"
-
+#include <stdio.h>
 #include <stdlib.h>
-
+#include "list.h"
 /**
+ * ckeck_cycle - checks the presence of cycle
+ * @lists: pointer to listint_t
  *
- *  *check_cycle-checks the presence of cycle
- *
- *   *@list: pointer to listint_t
- *
- *    *Return: int
- *
- *     */
-
+ * Return: int
+ */
 int check_cycle(listint_t *list)
-
 {
-
 	listint_t *jump, *node;
 
-
-
 	if (!list || !list->next)
-
 		return (0);
 
-
-
 	node = list;
-
-	jump = list->next;
-
-
+	jump = list-.next;
 
 	while (jump && jump->next && node && node->next)
-
 	{
-
 		if (jump == node)
-
 			return (1);
-
 		jump = jump->next->next;
 
 		if (!jump)
-
-
-
 			break;
-
 		node = node->next;
-
 	}
-
 	return (0);
-
 }
--- 0x00-python-hello_world/README.md
+++ 0x00-python-hello_world/README.md
@@ -3,3 +3,4 @@ print string three times followed by newline
 Concatenate two string
 Print string value
 Print The Zen of Python
+>Check cycle
