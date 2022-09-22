fb552102c42c020427d3a72fdef112a627d2fbbc
root <kariukionesmus01@gmail.com>
2022-09-22 13:10:35 -0700

    Import function from file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
 from add_0 import add
+
 if __name__ == '__main__':
     a = 1
     b = 2
-    c = a + b
+    c = add(a, b)
 
-    print('{} + {} = {}\n'.format(a, b, c))
+    print('{} + {} = {}'.format(a, b, c))
+
