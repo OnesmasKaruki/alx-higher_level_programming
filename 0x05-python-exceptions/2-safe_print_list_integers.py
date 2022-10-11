f15442ec288b3a19d816183ef215ed983c9f6d68
root <kariukionesmus01@gmail.com>
2022-10-11 14:15:59 -0700

    Print elem in a list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    len_elem = 0
+    a= 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i], end=''))
-            len_elem += 1
+            print("{:d}".format(my_list[i]), end='')
+            a += 1
         except (ValueError, TypeError):
             pass
-    print()
-    return len_elem
+        print()
+        return a
