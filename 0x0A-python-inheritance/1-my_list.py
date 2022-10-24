44788cd9ac9d9b66c437b0a8fd589cb2678aa79d
root <kariukionesmus01@gmail.com>
2022-10-24 11:02:45 -0700

    Inheritance finished

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Module: 1-my_list
+'''
+
+class Mylist(list):
+    '''Represent a Mylist
+    '''
+
+    def print_dorted(self):
+        '''print the list but sorted
+        '''
+        print9sorted((self))
