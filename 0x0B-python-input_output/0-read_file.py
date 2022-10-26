a8bd651500afede602b1a20081499faba817f436
root <kariukionesmus01@gmail.com>
2022-10-26 12:13:49 -0700

    Read funct

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,5 @@
 
 def read_file(filename=""):
     """Read text file"""
-    with open(filename, "r",  encoding = "utf-8") as f:
+    with open(filename, "r",  encoding="utf-8") as f:
         print(f.read(), end="")
