e5511fc6f0ddfe6dbe869a607ddc56a389c71206
OnesmasKaruki <kariukionesmus@gmail.com>
2022-11-30 00:04:45 +0300

    Create database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,6 +1,2 @@
---a script that creates the datav=base htbn_0c_0
---If the database htbn_0c_0 already exist it should not fail
---SELECT or show statement are forbiden
 -- creates databse if not exists
-
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
