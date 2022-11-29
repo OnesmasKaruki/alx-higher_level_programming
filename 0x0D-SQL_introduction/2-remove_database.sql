c9ad97e9e57ca13612cc7c877c0bfcd65a441e7e
OnesmasKaruki <kariukionesmus@gmail.com>
2022-11-29 23:47:28 +0300

    All

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,6 +1,2 @@
---a script that delete the database htbn_0c_0
---If the database doesn't exist script should not fail
---SELECT SHOW statement not allow
 -- creates databse if not exists
-
 DROP DATABASE IF EXISTS hbtn_0c_0;
